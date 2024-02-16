f5b45e1f90671dba6dd435a99549192176c5c13b
Iasonas <iasonas.ps@gmail.com>
2024-02-16 16:06:07 +0200

    Add project files.

---
+++ Narayana's cows sequence/Helpers/EuropeanΝumberFormat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narayana_s_cows_sequence.Helpers
+{
+    internal static class EuropeanΝumberFormat
+    {
+        public static string ChangeFormat(long number)
+        {
+            List<long> result = GetRemainder(number);
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in result)
+            {
+                sb.Append(item.ToString());
+                if (result.Last() != item)
+                {
+                    sb.Append('.');
+                }
+            }
+            return sb.ToString();
+        }
+        public static List<long> GetRemainder(long num)
+        {
+            //long length = num % 1000000 > 100 ? 4 : 8; 
+            //long[] longs = new long[length];
+            List<long> longs = new List<long>();
+
+            while (num > 1000)
+            {
+                longs.Add(num % 1000);
+                num = num / 1000;
+            }
+            longs.Add(num);
+            longs.Reverse();
+            return longs;
+        }
+    }
+}
---
+++ Narayana's cows sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿using Narayana_s_cows_sequence.Models;
+
+namespace Narayana_s_cows_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lets Count Cows :");
+            Console.WriteLine();
+            Sequence.GetSequence();
+        }
+    }
+}
